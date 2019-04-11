22e08c2cf09870ae9befb2f49bf7988178dffead
Julien <julien.kiang@umontreal.ca>
2019-04-10 22:06:24 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,10 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+    Stage stage;
     @Override
     public void start(Stage primaryStage) throws Exception{
+        stage=primaryStage;
         Parent root = FXMLLoader.load(getClass().getResource("UI/login.fxml"));
         primaryStage.setTitle("KijijiDEM");
         primaryStage.setScene(new Scene(root));
